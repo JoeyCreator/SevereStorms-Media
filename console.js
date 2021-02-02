2b3f5bfd449d8ecaa468c3c173da3159b0dd2815
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-02-02 14:45:14 +0000

    script disable

--- console.js
+++ console.js
@@ -5,3 +5,14 @@ var cssRule =
     "text-shadow: 1px 1px 5px rgb(249, 162, 34);" +
     "filter: dropshadow(color=rgb(249, 162, 34), offx=1, offy=1);";
 console.log("%cThis is a browser feature intended for developers", cssRule);
+
+
+
+
+var cssRule =
+    "color: red;" +
+    "font-size: 60px;" +
+    "font-weight: bold;" +
+    "text-shadow: 1px 1px 5px rgb(249, 162, 34);" +
+    "filter: dropshadow(color=rgb(249, 162, 34), offx=1, offy=1);";
+console.log("%cThis is a browser feature intended for developers", cssRule);
