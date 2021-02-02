b2b4d863d48da1af1789ebca54e1d68d1cc838e9
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-02-02 14:46:46 +0000

    js msg

--- console.js
+++ console.js
@@ -4,15 +4,15 @@ var cssRule =
     "font-weight: bold;" +
     "text-shadow: 1px 1px 5px rgb(249, 162, 34);" +
     "filter: dropshadow(color=rgb(249, 162, 34), offx=1, offy=1);";
-console.log("%cThis is a browser feature intended for developers", cssRule);
+console.log("%cThis is a browser feature intended for developers.", cssRule);
 
 
 
 
 var cssRule =
     "color: red;" +
-    "font-size: 60px;" +
+    "font-size: 40px;" +
     "font-weight: bold;" +
     "text-shadow: 1px 1px 5px rgb(249, 162, 34);" +
     "filter: dropshadow(color=rgb(249, 162, 34), offx=1, offy=1);";
-console.log("%cThis is a browser feature intended for developers", cssRule);
+console.log("%cDo not copy any code of this website.", cssRule);
