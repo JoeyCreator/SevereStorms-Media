f3d78071e850a101ed3011610a5eb1f3b6544997
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-02-03 14:48:22 +0000

    : adding modules

---
+++ modules/system/console.js
@@ -0,0 +1,11 @@
+/**
+ * @author Joey Cadieux <joeycadieux161@gmail.com>
+ */
+
+var cssRule =
+    "color: red;" +
+    "font-size: 40px;" +
+    "font-weight: bold;" +
+    "text-shadow: 1px 1px 5px rgb(249, 162, 34);" +
+    "filter: dropshadow(color=rgb(249, 162, 34), offx=1, offy=1);";
+console.log("%cStop", cssRule);
